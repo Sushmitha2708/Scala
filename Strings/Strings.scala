b44af07514897bd90de71da0423bb19ba383d9f1
Susmitha Mudigonda <51853078+Sushmitha2708@users.noreply.github.com>
2020-05-31 12:33:36 +0530

    Create Strings.scala

---
+++ Strings/Strings.scala
@@ -0,0 +1,11 @@
+object Strings{
+	def main(args: Array[String]){
+		val message : String = "Hey people!"
+		var m1= "Hakuna";
+		var m2="Matata";
+		var len = message.length();
+		println(message)
+		println("length of the message is"+ len);
+		println("The new message is"+m1+m2);
+	}
+}
